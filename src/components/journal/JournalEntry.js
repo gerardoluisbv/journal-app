cd60c62d627b99843c1ab3296f0e61e95b7d6f79
Gerardo <gerardoluisbv@gmail.com>
2022-01-05 23:07:54 -0500

    parte de los etilos del componente <Sidebar />

---
+++ src/components/journal/JournalEntry.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const JournalEntry = () => {
+    return (
+        <div className='journal__entry pointer'>
+            
+            <div 
+                className='journal__entry-picture'
+                style={{
+                    background:'cover',
+                    backgroundImage: 'url(https://www.akamai.com/content/dam/site/im-demo/perceptual-standard.jpg?imbypass=true)'
+                }}
+            ></div>
+
+        <div className='journal__entry-body'>
+                <p className='journal__entry-title'>
+                    Un nuevo dia
+                </p>
+                <p className='journal__entry-content'>
+                   Pariatur culpa dolore laborum amet tempor aliqua ullamco.
+                </p>
+        </div>
+
+        <div className='journal__entry-date-box'>
+                <span>Monday</span>
+                <h4>22</h4>
+        </div>
+
+        </div>
+    )
+}
