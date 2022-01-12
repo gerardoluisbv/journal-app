9b3f3fd356d6cece52cb2b387fd2390b90842ae5
Gerardo <gerardoluisbv@gmail.com>
2022-01-11 21:44:09 -0500

    Estructura y diseño del Componente <HomeScreen />

--- src/components/notes/NoteScreen .js
+++ src/components/notes/NoteScreen .js
@@ -7,6 +7,30 @@ export const NoteScreen  = () => {
             
             <NotesAppBar />
 
+            <div className='notes__content'>
+
+                <input 
+                    type="text"
+                    placeholder='Some awesome title'
+                    className='notes__title-input'
+                    autoComplete='off'
+                />
+
+                <textarea
+                    placeholder='What happened today'
+                    className='notes__textarea'
+                ></textarea>
+
+                <div className='notes__image'>
+                    <img 
+                        src='https://photographycourse.net/wp-content/uploads/2014/11/Landscape-Photography-steps.jpg'
+                        alt='imagen'
+                    />
+                </div>
+
+            </div>
+            
+
         </div>
     )
 }
