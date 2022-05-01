7d44288290ba3213ca487986057af7cc996e1359
Gerardo <gerardoluisbv@gmail.com>
2022-05-01 17:58:51 -0500

    firebase config

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -50,27 +50,13 @@ const firebaseConfig = {
   firebase.default.initializeApp(firebaseConfig);
   const db = firebase.firestore();
   const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
- 
-  const config = {
-    env : {
-      FIREBASE_API_KEY: process.env.apiKey,
-      AUTH_DOMAIN: process.env.authDomain,
-      PROJECT_ID: process.env.projectId,
-      STORAGE_BUCKET: process.env.storageBucket,
-      MESSAGING_SENDER_ID: process.env.messagingSenderId,
-      APP_ID: process.env.appId,
-    }
-  }
-
 
   export {
       db,
       googleAuthProvider,
-      firebase,
-      config
+      firebase
   }
 
-  
 
 
 
